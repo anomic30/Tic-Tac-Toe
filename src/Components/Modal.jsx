26191b63c6c8a0bf04ba2e39582f9b942da6205f
[Anom Chakravorty] <anomchakravorty3008@gmail.com>
2021-07-03 01:57:43 +0530

    added win screen

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Modal = ({winner, showModal}) => {
     return (
         <div className="container">
-            <h1>Winner: </h1>
+            <h1>Winner</h1>
             <h1>{winner}</h1>
-            <button onClick={()=>showModal()}>Play again</button>
+            <button onClick={()=>showModal()}>Play again!</button>
         </div>
     )
 }
